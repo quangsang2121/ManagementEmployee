7d60b50bf1ec9644fce283c4a30d047205ae3ab7
sbui8 <sbui8@dxc.com>
2022-09-19 00:39:07 +0700

    back end

--- src/main/java/com/sang/ManageEmployee/model/Employee.java
+++ src/main/java/com/sang/ManageEmployee/model/Employee.java
@@ -9,7 +9,7 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
-@Data
+//@Data
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -26,4 +26,51 @@ public class Employee {
 	
 	@Column(name = "email_id")
 	private String emailId;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Employee( String firstName, String lastName, String emailId) {
+		super();
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
