e0f77ae7f6afa0c2aaf9e93af5fa96bea09a6f0d
quang sang <quangsang2121@gmail.com>
2022-09-25 10:46:19 +0700

    front end

--- src/main/java/com/sang/ManageEmployee/service/EmployeeService.java
+++ src/main/java/com/sang/ManageEmployee/service/EmployeeService.java
@@ -2,14 +2,20 @@ package com.sang.ManageEmployee.service;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.sang.ManageEmployee.model.Employee;
 
 
-public interface EmployeeService{
+public interface EmployeeService {
 	public List<Employee> getAllEmployee();
 	
 	public Employee createEmployee(Employee em);
+	
+	public ResponseEntity<Employee> getEmployeeById(Long id);
+	
+	public ResponseEntity<Employee> updateEmployee(Long id, Employee empDetails);
 	
 }
--- src/main/java/com/sang/ManageEmployee/service/ImpEmployeeService.java
+++ src/main/java/com/sang/ManageEmployee/service/ImpEmployeeService.java
@@ -3,8 +3,10 @@ package com.sang.ManageEmployee.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.sang.ManageEmployee.exception.ResourceNotFoundException;
 import com.sang.ManageEmployee.model.Employee;
 import com.sang.ManageEmployee.repository.EmployeeRepository;
 @Service
@@ -24,6 +26,25 @@ public class ImpEmployeeService implements EmployeeService{
 		// TODO Auto-generated method stub
 		
 		return employeeRepository.save(em);
+	}
+
+	@Override
+	public ResponseEntity<Employee> getEmployeeById(Long id) {
+		// TODO Auto-generated method stub
+		Employee empl = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exits with id:"+id));
+		return ResponseEntity.ok(empl);
+	}
+
+	@Override
+	public ResponseEntity<Employee> updateEmployee(Long id, Employee empDetails) {
+		// TODO Auto-generated method stub
+		Employee empl = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exits with id:"+id));
+		
+		empl.setFirstName(null);
+		empl.setLastName(null);
+		empl.setEmailId(null);
+		
+		return null;
 	}
 	
 	
