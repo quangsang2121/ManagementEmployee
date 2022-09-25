15948de5d269f7ebed95be0ba84d2da512388afa
quang sang <quangsang2121@gmail.com>
2022-09-25 10:47:09 +0700

    front end

---
+++ src/main/java/com/sang/ManageEmployee/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.sang.ManageEmployee.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sang.ManageEmployee.model.Employee;
+import com.sang.ManageEmployee.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	//@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployee() {
+		return employeeService.getAllEmployee();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee em) {
+		// TODO Auto-generated method stub
+		return employeeService.createEmployee(em);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		// TODO Auto-generated method stub
+		return employeeService.getEmployeeById(id);
+	}
+	
+	
+}
